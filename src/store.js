fa0a3e18dd60cc643f691fb1cadf839a993db9b3
twana koye <twana.00569020@gmail.com>
2024-05-22 21:01:14 +0300

    version1.1

--- src/store.js
+++ src/store.js
@@ -1,8 +1,53 @@
 import { create } from "zustand";
+import { produce } from "immer";
+
+const initialState = {
+  user: {
+    id: "user123",
+    friends: ["jack", "jessica", "colin", "paulo"],
+    profile: {
+      name: "twana",
+      email: "twana@gmail.com",
+      address: {
+        street: "123 mainst",
+        city: "oldTown",
+        zipcode: "12345",
+      },
+    },
+  },
+};
+export const useStore2 = create((set) => ({
+  ...initialState,
+  updateAdressStreet: (street) =>
+    set(
+      produce((state) => {
+        state.user.profile.address.street = street;
+      })
+    ),
+}));
+
+export const useStore = create((set) => ({
+  ...initialState,
+  updateAdressStreet: (street) =>
+    set((state) => ({
+      user: {
+        ...state.user,
+        profile: {
+          ...state.user.profile,
+          address: { ...state.user.profile.address, street: street },
+        },
+      },
+    })),
+}));
 
 const useCart = create((set) => ({
   cart: [],
-  addToCart: (product) => set((state) => ({ cart: [...state.cart, product] })),
+  addToCart: (product) =>
+    set((state) => {
+      if (!state.cart.some((item) => item.id === product.id))
+        return { cart: [...state.cart, product] };
+      return state;
+    }),
   removeFromCart: (productID) =>
     set((state) => ({
       cart: state.cart.filter((product) => product.id !== productID),
