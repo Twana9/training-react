adb8321b3935a603569bfe4b836b16261d25daeb
twana koye <twana.00569020@gmail.com>
2024-06-26 15:26:52 +0300

    aa

--- src/ApiFetching/SinglePost.jsx
+++ src/ApiFetching/SinglePost.jsx
@@ -1,22 +1,21 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 export default function SinglePost() {
-  const [postComment, setPostComment] = useState([]);
+  const [postComment, setPostComment] = useState({});
   const [selectedID, setSelectedID] = useState(1);
   const [error, setError] = useState(null);
   const [idFromButton, setIdFromButton] = useState(1);
 
   useEffect(() => {
     axios
-      .get(
-        `https://jsonplaceholder.typicode.com/posts/${idFromButton}/comments`
-      )
+      .get(`https://jsonplaceholder.typicode.com/posts/1/comments`)
       .then((res) => {
-        console.log(res.data);
-        setPostComment(res.data);
+        const comments = res.data;
+        const comment = comments.find((comment) => comment.id === idFromButton);
+        setPostComment(comment);
+        setError(null);
       })
       .catch((err) => {
-        console.log(err);
         setError(err.message);
       });
   }, [idFromButton]);
@@ -39,18 +38,23 @@ export default function SinglePost() {
         <p>{error}</p>
       ) : (
         <div>
-          <p>Post Comments</p>
+          <p>Post Comment</p>
           <br />
-          <ul>
-            {postComment.map((c) => (
-              <li key={c.id}>
-                <p>{c.name}</p>
-                <p>{c.body}</p>
-              </li>
-            ))}
-          </ul>
+          <p>{postComment.name}</p>
+          <p>{postComment.body}</p>
         </div>
       )}
     </>
   );
+}
+
+{
+  /* <ul>
+  {postComment.map((c) => (
+    <li key={c.id}>
+      <p>{c.name}</p>
+      <p>{c.body}</p>
+    </li>
+  ))}
+</ul> */
 }
