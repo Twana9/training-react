df9f4c55cdee1a51d15a1d20c1c127b77cc00158
twana koye <twana.00569020@gmail.com>
2024-07-03 15:29:57 +0300

    search params

--- src/Pages/BookLayout.jsx
+++ src/Pages/BookLayout.jsx
@@ -1,18 +1,26 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useSearchParams } from "react-router-dom";
 
 export function BookLayout() {
+  const [searchParams, setSearchParams] = useSearchParams({ n: 3 });
+  const number = searchParams.get("n");
   return (
     <>
       <Link to="/books/1">book 1</Link>
       <br />
       <Link to="/books/2">book 2</Link>
       <br />
+      <Link to={`/books/${number}`}>book {number}</Link>
+      <br />
       <Link to="/books/new">book new</Link>
       <Outlet context={{ company: "astepor" }} />
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setSearchParams({ n: e.target.value })}
+      />
     </>
   );
 }
-
 // <Link to="/books/1" replace>
 //   Book 1
 // </Link>
