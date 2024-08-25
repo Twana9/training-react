ed40c57b2ac6368e5f43b4690344c5fe97998d3b
twana koye <twana.00569020@gmail.com>
2024-08-25 15:33:11 +0300

    new animation

--- src/Framer-Motion/ButtonInView.jsx
+++ src/Framer-Motion/ButtonInView.jsx
@@ -1,47 +1,74 @@
-import { useInView } from "framer-motion";
+import { useInView, motion } from "framer-motion";
 import { useRef } from "react";
 
 export default function ButtonInView() {
-  const ref = useRef(null);
-  const isInView = useInView(ref);
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+  const ref3 = useRef(null);
+
+  const isInView1 = useInView(ref1);
+  const isInView2 = useInView(ref2);
+  const isInView3 = useInView(ref3);
+
+  const isAnyInView = isInView1 || isInView2 || isInView3;
+
   return (
     <div
       style={{
         position: "relative",
       }}
     >
-      {isInView && (
+      {isAnyInView && (
         <motion.button
-          style={{ position: "fixed", bottom: "10vh", right: "5vw" }}
+          onClick={() => scrollTo(0, 0)}
+          style={{
+            position: "fixed",
+            bottom: "10vh",
+            right: "5vw",
+            borderRadius: "50%",
+            padding: "20px",
+            fontSize: "20px",
+            background: "green",
+            color: "white",
+            outline: "none",
+            border: "none",
+          }}
         >
           Top
         </motion.button>
       )}
       <div
         style={{
-          height: "10vh",
+          height: "180vh",
+          background: "purple",
         }}
-      ></div>
+      >
+        {" "}
+        first
+      </div>
       <div
-        ref={ref}
+        ref={ref1}
         style={{
           height: "100vh",
+          background: "yellow",
         }}
       >
         1
       </div>
       <div
-        ref={ref}
+        ref={ref2}
         style={{
           height: "100vh",
+          background: "lightBlue",
         }}
       >
         2
       </div>
       <div
-        ref={ref}
+        ref={ref3}
         style={{
           height: "100vh",
+          background: "pink",
         }}
       >
         3
